a5c8a4b3e8d50a9dce976e168da4251739b05b33
KuKetto <39841262+KuKetto@users.noreply.github.com>
2022-05-17 23:17:54 +0200

    Delete uni-graph-lb directory

--- uni-graph-lb/src/controllers/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './universities.controller';
-export * from './user.controller';
-export * from './templates.controller';
-export * from './subject.controller';
-export * from './subject-detail.controller';
-export * from './userdata.controller';
